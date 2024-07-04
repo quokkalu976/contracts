2d7944c9479cdba7b549fb39e9e50aad57011644
quokkalu976 <quokkalu976@rolldex.io>
2024-07-04 10:14:35 +0000

    add access control

--- contracts/diamond/facets/DiamondCutFacet.sol
+++ contracts/diamond/facets/DiamondCutFacet.sol
@@ -4,6 +4,7 @@ pragma solidity ^0.8.19;
 import "../../utils/Constants.sol";
 import "../interfaces/IDiamondCut.sol";
 import "../libraries/LibDiamond.sol";
+import "../libraries/LibAccessControlEnumerable.sol";
 
 contract DiamondCutFacet is IDiamondCut {
 
@@ -11,6 +12,7 @@ contract DiamondCutFacet is IDiamondCut {
      * diamondCut((address,uint8,bytes4[])[],address,bytes)
      */
     function diamondCut(FacetCut[] calldata _diamondCut, address _init, bytes calldata _calldata) external override {
+	LibAccessControlEnumerable.checkRole(Constants.DEPLOYER_ROLE);    
         LibDiamond.diamondCut(_diamondCut, _init, _calldata);
     }
 }
